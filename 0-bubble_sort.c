abe12b6148a162b3e3248a67a6723b1b35585f7f
Sarolus <cactussius.primus@gmail.com>
2021-06-14 14:50:29 +0200

    Add size condition

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,6 +11,9 @@ void bubble_sort(int *array, size_t size)
 	size_t i = 0, j = 0;
 	int tmp;
 
+	if (size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
